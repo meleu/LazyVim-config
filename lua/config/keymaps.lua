2492afe1689876f3fa3455cd72a75fb770e3b660
Augusto Lopes <meleu.dev@gmail.com>
2024-04-13 03:53:47 -0300

    disable H and L to navigate between buffers

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+----------------------------------------------------
+-- meleu: H and L have a native meaning in vim and should not be remapped to
+-- do other things. These are keymaps I use a lot in other apps that have
+-- vim keybindings (like tmux and Obsidian).
+vim.keymap.del("n", "<S-h>")
+vim.keymap.del("n", "<S-l>")
