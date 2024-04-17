b5e8fdc0096f25092df70e048ab0751de0860d44
meleu <8508804+meleu@users.noreply.github.com>
2024-04-17 13:48:12 -0300

    disable conceallevel for some filetypes

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- show "raw" contents for these filetypes
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "json", "jsonc", "hurl" },
+  callback = function()
+    vim.wo.conceallevel = 0
+  end,
+})
