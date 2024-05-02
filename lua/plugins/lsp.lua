47915974574371bbf66486b613e0e231a0c3959a
meleu <8508804+meleu@users.noreply.github.com>
2024-05-01 22:41:40 -0300

    rubocop linting and format on save

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -10,5 +10,26 @@ return {
 
     -- use gR to "Go to References"
     keys[#keys + 1] = { "gR", ":Telescope lsp_references<cr>", desc = "[G]oto [R]eferences" }
+
+    -- config taken from:
+    -- https://docs.rubocop.org/rubocop/usage/lsp.html#neovim-nvim-lspconfig
+    vim.opt.signcolumn = "yes"
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "ruby",
+      callback = function()
+        vim.lsp.start({
+          name = "rubocop",
+          cmd = { "bundle", "exec", "rubocop", "--lsp" },
+        })
+      end,
+    })
+    -- format on save
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*.rb",
+      callback = function()
+        vim.lsp.buf.format()
+      end,
+    })
+    ------------------------------------------------------------------------
   end,
 }
