d34ac0ee79232df888aa56008c2193293fba88ec
meleu <8508804+meleu@users.noreply.github.com>
2024-07-09 13:25:14 -0300

    disable json and yaml conceallevel

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Disable the concealing in some file formats
+-- This "conceal" thing omits quotes.
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "json", "jsonc", "yaml", "hurl" },
+  callback = function()
+    vim.wo.conceallevel = 0
+  end,
+})
