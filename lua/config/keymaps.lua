daf9f44f8ac823a719b9da0905371085a88eaf47
meleu <8508804+meleu@users.noreply.github.com>
2024-07-09 13:22:49 -0300

    navigate between buffers like they were tabs

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- These keys have a native meaning in vim and should be remapped!
+vim.keymap.del("n", "<S-h>")
+vim.keymap.del("n", "<S-l>")
+
+-- since LazyVim comes with bufferline, making buffers look like tabs,
+-- I want to navigate between buffers with gt/gT.
+vim.keymap.set("n", "gt", ":BufferLineCycleNext<CR>")
+vim.keymap.set("n", "gT", ":BufferLineCyclePrev<CR>")
