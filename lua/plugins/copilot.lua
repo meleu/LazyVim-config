216e10f0153b524bfce765b8183c644969286c65
meleu <8508804+meleu@users.noreply.github.com>
2024-07-09 13:37:22 -0300

    keymaps to enable/disable GitHub Copilot

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,8 @@
+return {
+  "zbirenbaum/copilot.lua",
+  keys = {
+    { "<leader>cpd", ":Copilot disable<cr>", { desc = "GitHub Copilot Disable" } },
+    { "<leader>cpe", ":Copilot enable<cr>", { desc = "GitHub Copilot Enable" } },
+    -- TODO: find a way to toggle copilot
+  },
+}
