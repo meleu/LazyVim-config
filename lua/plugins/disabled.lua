67a7cbfd0b2d3b572ef3a459d7ad6f57bd204f77
meleu <8508804+meleu@users.noreply.github.com>
2024-07-09 13:13:48 -0300

    disable mini.surround, install "mandatory" plugins, configure gr/gR

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,5 @@
+return {
+  -- disable mini.surround, (confusing keybindings)
+  -- I have years of muscle memory using tpope/vim-surround
+  { "echasnovski/mini.surround", enabled = false },
+}
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,13 @@
+-- overriding LazyVim's default LSP configs
+return {
+  "neovim/nvim-lspconfig",
+  opts = function()
+    local keys = require("lazyvim.plugins.lsp.keymaps").get()
+
+    -- I want to use gr for "Go Replace" (vim-scripts/ReplaceWithRegister)
+    keys[#keys + 1] = { "gr", false }
+
+    -- use gR to "Go to References"
+    keys[#keys + 1] = { "gR", ":Telescope lsp_references<cr>", desc = "[G]oto [R]eferences" }
+  end,
+}
