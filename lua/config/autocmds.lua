7441be5fcaf32fdeb0c43f77b9adbd0dc9a231f3
meleu <meleu.dev@gmail.com>
2024-07-22 23:19:45 -0300

    disable json and yaml "renderization"

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- disable the concealing in some file formats
+-- (this "conceal" thing omits quotes, which is important in JSON)
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "json", "jsonc", "yaml" },
+  callback = function()
+    vim.wo.conceallevel = 0
+  end,
+})
