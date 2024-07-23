40e7e441938b0b68fd3cf69217056876b35ff905
meleu <meleu.dev@gmail.com>
2024-07-22 22:23:45 -0300

    disable `<cr>` to accept autosuggestions

---
+++ lua/plugins/extended-nvim-cmp.lua
@@ -0,0 +1,16 @@
+return {
+  "hrsh7th/nvim-cmp",
+  opts = function(_, opts)
+    -- disable <cr> to accept completion
+    -- enable <tab> and <S-tab> to navigate completion menu
+    local cmp = require("cmp")
+    opts.mapping = vim.tbl_extend("force", opts.mapping, {
+      ["<Tab>"] = cmp.mapping.select_next_item(),
+      ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+      ["<cr>"] = function(fallback)
+        cmp.abort()
+        fallback()
+      end,
+    })
+  end,
+}
