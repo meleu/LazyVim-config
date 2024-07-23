5507af9349b5c51207de956206950e56e46c5c64
meleu <meleu.dev@gmail.com>
2024-07-22 22:05:05 -0300

    install basic "mandatory" plugins

---
+++ lua/plugins/basic-plugins.lua
@@ -0,0 +1,6 @@
+return {
+  "vim-scripts/ReplaceWithRegister",
+  "tpope/vim-surround",
+  "tpope/vim-repeat", -- make vim-surround dot-repeatable
+  "tpope/vim-speeddating", -- <C-a>/<C-x> to increase/decrease dates
+}
