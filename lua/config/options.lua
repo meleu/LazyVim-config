db4a0e5cff609dbf5fa784308c32c058d1ebabcc
meleu <meleu.dev@gmail.com>
2024-07-22 21:58:45 -0300

    bootstrap lazyvim config and load my ~/.vimrc

---
+++ lua/config/options.lua
@@ -0,0 +1,8 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- lad my own "old" configs written in VimScript
+if vim.fn.filereadable("~/.vimrc") then
+  vim.cmd("source ~/.vimrc")
+end
