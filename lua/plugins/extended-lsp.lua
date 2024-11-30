5f97a665e7d49ad524c50280fd3fe946a7bfc6e0
meleu <meleu.dev@gmail.com>
2024-11-29 21:18:02 -0300

    LSP ruby configs

--- lua/plugins/extended-lsp.lua
+++ lua/plugins/extended-lsp.lua
@@ -1,6 +1,8 @@
 return {
   "neovim/nvim-lspconfig",
-  opts = function()
+  opts = function(_, opts)
+    -- keymaps
+    ------------------------------------------------------------------------
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
 
     -- I want to use gr for "Go Replace" (ReplaceWithRegister plugin)
@@ -11,5 +13,26 @@ return {
 
     -- use gh to "hover documentation"
     keys[#keys + 1] = { "gh", vim.lsp.buf.hover, desc = "Hover" }
+
+    -- ruby stuff
+    ------------------------------------------------------------------------
+    local lspconfig = require("lspconfig")
+    opts = {
+      servers = {
+        rubocop = {
+          cmd = { "bundle", "exec", "rubocop", "--lsp" },
+          root_dir = lspconfig.util.root_pattern("Gemfile", ".git", "."),
+        },
+        solargraph = {
+          autoformat = true,
+          completion = true,
+          diagnostic = true,
+          folding = true,
+          references = true,
+          rename = true,
+          symbols = true,
+        },
+      },
+    }
   end,
 }
