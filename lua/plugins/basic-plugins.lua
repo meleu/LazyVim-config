3705158a969b5584fb63815b2561ca7993af71ad
meleu <meleu.dev@gmail.com>
2024-11-29 21:14:51 -0300

    add vim-rails

--- lua/plugins/basic-plugins.lua
+++ lua/plugins/basic-plugins.lua
@@ -2,5 +2,6 @@ return {
   { "vim-scripts/ReplaceWithRegister", vscode = true },
   { "tpope/vim-surround", vscode = true },
   { "tpope/vim-repeat", vscode = true }, -- make vim-surround dot-repeatable
-  { "tpope/vim-speeddating" }, -- <C-a>/<C-x> to increase/decrease dates
+  { "tpope/vim-speeddating", vscode = true }, -- <C-a>/<C-x> to increase/decrease dates
+  { "tpope/vim-rails", vscode = true }, -- awesome for Ruby on Rails
 }
