1e1d51c97c5e1ae5feefc6bc96e620b77a4e459c
meleu <meleu.dev@gmail.com>
2024-12-03 08:59:07 -0300

    Add keymaps to yank filepath

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,16 +8,27 @@
 vim.keymap.del("n", "<S-h>")
 vim.keymap.del("n", "<S-l>")
 
+-- copy file path to the clipboard
+-- source: https://stackoverflow.com/a/17096082/6354514
+vim.keymap.set("n", "<leader>yf", ':let @+ = expand("%")<cr>', {
+  desc = "Yank relative file path",
+})
+vim.keymap.set("n", "<leader>yF", ':let @+ = expand("%:p")<cr>', {
+  desc = "Yank full file path",
+})
+
+---------------------------
 --[ VSCode only configs ]--
+---------------------------
 if vim.g.vscode then
   -- the "close current buffer" in VSCode is kinda buggy
   vim.keymap.del("n", "<leader>bd")
   return
 end
 
------------------------------------------------------------------------
---[ put below this line only the keymaps you do NOT want in VSCode. ]--
------------------------------------------------------------------------
+---------------------------------------------------
+--[ explicit non-VSCode keymaps below this line ]--
+---------------------------------------------------
 
 -- since LazyVim comes with bufferline, making buffers look
 -- like tabs, I want to navigate between buffers with gt/gT.
