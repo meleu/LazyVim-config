f5753316c5010dfa7a8599c43288497238f997b8
meleu <meleu.dev@gmail.com>
2025-02-08 20:28:24 -0300

    Add custom keymaps and disable conflicting ones
    - Disable H and L for buffer navigation to preserve default Vim behavior.
    - Add keymaps to copy file paths to clipboard.
    - Remove specific keymaps when running in VSCode to avoid conflicts.
    - Set keymaps for buffer navigation using gt/gT with LazyVim.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,47 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+---------------------------------------------------------------------------
+-- disable H and L to navigate between buffers.
+---------------------------------------------------------------------------
+-- reasoning: H and L have a default Vim behavior
+-- and should never be overwritten
+vim.keymap.del("n", "<S-h>")
+vim.keymap.del("n", "<S-l>")
+
+---------------------------------------------------------------------------
+-- copy file path to clipboard
+---------------------------------------------------------------------------
+-- source: https://stackoverflow.com/a/17096082/6354514
+vim.keymap.set("n", "<leader>fy", ':let @+ = expand("%")<cr>', {
+  desc = "Yank relative file path",
+})
+vim.keymap.set("n", "<leader>fY", ':let @+ = expand("%:p")<cr>', {
+  desc = "Yank full file path",
+})
+
+---------------------------
+--[ VSCode only configs ]--
+---------------------------
+if vim.g.vscode then
+  -- the "close current buffer" in VSCode is kinda buggy
+  vim.keymap.del("n", "<leader>bd")
+
+  -- disable terminal when in VSCode
+  vim.keymap.del("n", "<C-/>")
+
+  -- disable lazygit
+  vim.keymap.del("n", "<leader>gg")
+
+  return
+end
+
+---------------------------------------------------
+--[ explicit non-VSCode keymaps below this line ]--
+---------------------------------------------------
+
+-- since LazyVim comes with bufferline, making buffers look
+-- like tabs, I want to navigate between buffers with gt/gT.
+vim.keymap.set("n", "gt", ":bnext<cr>", { desc = "Next buffer" })
+vim.keymap.set("n", "gT", ":bprevious<cr>", { desc = "Prev buffer" })
