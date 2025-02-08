87bc71fc40d513472c8501552adac502a9fe4a38
meleu <meleu.dev@gmail.com>
2025-02-08 20:24:40 -0300

    Add custom VimScript configs and filetype settings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,29 @@
 -- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Default options that are always set:
+-- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- load my own "old" configs written in VimScript
+if vim.fn.filereadable("~/.vimrc") then
+  vim.cmd("source ~/.vimrc")
+end
+
+-- consider BATS files as shell scripts
+vim.filetype.add({
+  extension = { bats = "sh" },
+  -- TODO:: consider Dangerfile as Ruby
+})
+
+-- LSP Server to use for Ruby
+-- set to "ruby_lsp" to use it instead of "solargraph"
+vim.g.lazyvim_ruby_lsp = "ruby_lsp"
+-- vim.g.lazyvim_ruby_lsp = "solargraph"
+vim.g.lazyvim_ruby_formatter = "standardrb"
+-- vim.g.lazyvim_ruby_formatter = "rubocop"
+
+-- disable animations
+vim.g.snacks_animate = false
+
+if vim.g.neovide then
+  vim.o.guifont = "CaskaydiaMono Nerd Font Mono:h14"
+end
