d45294741c9e33471a0f34376c56c27b7498d2b4
meleu <meleu.dev@gmail.com>
2025-02-10 00:05:05 -0300

    Disable concealing for JSON and YAML file types

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- disable the concealing in some file formats
+-- (this "conceal" thing omits quotes, which is important in JSON)
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "json", "jsonc", "yaml" },
+  callback = function()
+    vim.wo.conceallevel = 0
+  end,
+})
