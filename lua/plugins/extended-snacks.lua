f870960c98f2634a19f87f6e90694d4655438652
meleu <meleu.dev@gmail.com>
2025-02-21 09:53:33 -0300

    zen-mode: do not show the code in background

--- lua/plugins/extended-snacks.lua
+++ lua/plugins/extended-snacks.lua
@@ -1,6 +1,13 @@
 return {
   "folke/snacks.nvim",
   opts = {
+    zen = {
+      -- make the buffer behind the zen buffer to not be shown
+      win = { backdrop = { transparent = false } },
+      -- if you still wants transparency, adjust the transparency level
+      -- via blend (the lesser, the darker)
+      -- win = { backdrop = { transparent = true, blend = 10 } },
+    },
     dashboard = {
       preset = {
         -- ANSI art from: https://patorjk.com/software/taag/#p=display&f=Delta%20Corps%20Priest%201&t=meleu
