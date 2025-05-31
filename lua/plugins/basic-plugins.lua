cb4a4b03557dd9914a819b1dc8972fd31d39fafd
meleu <meleu.dev@gmail.com>
2025-05-31 08:44:42 -0300

    Update

--- lua/plugins/basic-plugins.lua
+++ lua/plugins/basic-plugins.lua
@@ -4,4 +4,5 @@ return {
   { "tpope/vim-repeat", vscode = true }, -- make vim-surround dot-repeatable
   { "tpope/vim-speeddating", vscode = true }, -- <C-a>/<C-x> to increase/decrease dates
   { "tpope/vim-rails", vscode = true }, -- awesome for Ruby on Rails
+  { "tpope/vim-dispatch", vscode = true }, -- useful to work with vim-rails
 }
