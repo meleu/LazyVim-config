42d73c8aa6cc7da8168cdc9935cd1407a204ed59
meleu <meleu.dev@gmail.com>
2025-09-21 17:17:47 -0300

    Use 'gR' to open LSP references with snacks_picker

--- lua/plugins/extended-lsp.lua
+++ lua/plugins/extended-lsp.lua
@@ -1,6 +1,7 @@
 return {
   "neovim/nvim-lspconfig",
   opts = function()
+    ------------------------------------------------------------------------
     -- keymaps
     ------------------------------------------------------------------------
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
@@ -9,8 +10,14 @@ return {
     keys[#keys + 1] = { "gr", false }
 
     -- use gR to "Go to References"
-    -- NOTE: I still didn't figure how to show the list in the snacks picker
-    keys[#keys + 1] = { "gR", vim.lsp.buf.references, desc = "References", nowait = true }
+    keys[#keys + 1] = {
+      "gR",
+      function()
+        Snacks.picker.lsp_references()
+      end,
+      nowait = true,
+      desc = "References",
+    }
 
     -- use gh to "hover documentation"
     keys[#keys + 1] = { "gh", vim.lsp.buf.hover, desc = "Hover" }
