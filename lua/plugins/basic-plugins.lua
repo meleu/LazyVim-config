478e409d480730aca81e98c47328345e4f892d32
meleu <meleu.dev@gmail.com>
2025-09-21 17:16:16 -0300

    Use a more recent replaceWithRegister plugin

--- lua/plugins/basic-plugins.lua
+++ lua/plugins/basic-plugins.lua
@@ -1,5 +1,5 @@
 return {
-  { "vim-scripts/ReplaceWithRegister", vscode = true },
+  { "inkarkat/vim-ReplaceWithRegister", vscode = true },
   { "tpope/vim-surround", vscode = true },
   { "tpope/vim-repeat", vscode = true }, -- make vim-surround dot-repeatable
   { "tpope/vim-speeddating", vscode = true }, -- <C-a>/<C-x> to increase/decrease dates
