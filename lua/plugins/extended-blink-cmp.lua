fcf44327b8423c683d4454776e0692e653501345
meleu <meleu.dev@gmail.com>
2025-09-21 17:17:11 -0300

    Make <Enter> mean an actual <Enter>
    And not a key to accept the autocompletion suggestion.

---
+++ lua/plugins/extended-blink-cmp.lua
@@ -0,0 +1,10 @@
+return {
+  "saghen/blink.cmp",
+  -- NOTE: config added so <Enter> means an actual <Enter>
+  -- (and not accepting a completion)
+  opts = {
+    keymap = {
+      preset = "default",
+    },
+  },
+}
