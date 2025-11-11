c0a2919ea4b9def0ed42d40663a5d93ac40cbbc6
meleu <meleu.dev@gmail.com>
2025-11-11 17:53:16 -0300

    Update lsp keybindings config to the new notation

--- lua/plugins/extended-lsp.lua
+++ lua/plugins/extended-lsp.lua
@@ -1,25 +1,26 @@
 return {
   "neovim/nvim-lspconfig",
-  opts = function()
-    ------------------------------------------------------------------------
-    -- keymaps
-    ------------------------------------------------------------------------
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
+  opts = {
+    servers = {
+      ["*"] = {
+        keys = {
+          -- I want to use gr for "Go Replace" (ReplaceWithRegister plugin)
+          { "gr", false },
 
-    -- I want to use gr for "Go Replace" (ReplaceWithRegister plugin)
-    keys[#keys + 1] = { "gr", false }
+          -- use gR to "Go to References"
+          {
+            "gR",
+            function()
+              Snacks.picker.lsp_references()
+            end,
+            nowait = true,
+            desc = "References",
+          },
 
-    -- use gR to "Go to References"
-    keys[#keys + 1] = {
-      "gR",
-      function()
-        Snacks.picker.lsp_references()
-      end,
-      nowait = true,
-      desc = "References",
-    }
-
-    -- use gh to "hover documentation"
-    keys[#keys + 1] = { "gh", vim.lsp.buf.hover, desc = "Hover" }
-  end,
+          -- use gh to "hover documentation"
+          { "gh", vim.lsp.buf.hover, desc = "Hover" },
+        },
+      },
+    },
+  },
 }
