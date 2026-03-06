e607d0bcb8ef2fbff021bebbf86c37e7a882b5eb
meleu <meleu@noreply.github.com>
2026-03-06 08:24:02 -0300

    Cosmetic changes

--- lua/config/neovide-options.lua
+++ lua/config/neovide-options.lua
@@ -1,5 +1,5 @@
 vim.o.guifont = "CaskaydiaMono Nerd Font Mono:h14"
 -- vim.g.neovide_transparency = 0.9
-vim.g.neovide_opacity = 0.7
+vim.g.neovide_opacity = 0.9
 -- disable animations when in neovide
 vim.g.snacks_animate = false
