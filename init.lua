da42b1c34b1ec2c2a1645338ee9ceec25a57d3ed
meleu <8508804+meleu@users.noreply.github.com>
2026-07-03 13:18:00 -0300

    Tweak some configs

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- following the instructions from:
+-- https://herb-tools.dev/integrations/editors/neovim
+-- require("lspconfig").herb_ls.setup()
