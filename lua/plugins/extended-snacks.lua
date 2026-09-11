e982d7884002960c9fb8687f0ed3b2597b626e84
meleu <8508804+meleu@users.noreply.github.com>
2026-09-10 22:35:37 -0300

    Temporary solution for Snacks.gitbrowse bug

--- lua/plugins/extended-snacks.lua
+++ lua/plugins/extended-snacks.lua
@@ -1,6 +1,21 @@
 return {
   "folke/snacks.nvim",
   vscode = false,
+  -- TODO: remove when https://github.com/folke/snacks.nvim/pull/2955 is merged
+  init = function()
+    vim.api.nvim_create_autocmd("User", {
+      -- trying to make sure this patch runs after snacks has loaded
+      pattern = "VeryLazy",
+      once = true,
+      callback = function()
+        local gitbrowse = require("snacks.gitbrowse")
+        local get_repo = gitbrowse.get_repo
+        gitbrowse.get_repo = function(...)
+          return (get_repo(...):gsub("^https://(https?://)", "%1"))
+        end
+      end,
+    })
+  end,
   opts = {
     zen = {
       -- make the buffer behind the zen buffer to not be shown
@@ -19,14 +34,14 @@ return {
         -- ]],
         -- https://patorjk.com/software/taag/?p=display&f=DOS%20Rebel&t=meleu&x=none
         header = [[
-                          ████                     
-                         ░░███                     
+                          ████
+                         ░░███
  █████████████    ██████  ░███   ██████  █████ ████
-░░███░░███░░███  ███░░███ ░███  ███░░███░░███ ░███ 
- ░███ ░███ ░███ ░███████  ░███ ░███████  ░███ ░███ 
- ░███ ░███ ░███ ░███░░░   ░███ ░███░░░   ░███ ░███ 
+░░███░░███░░███  ███░░███ ░███  ███░░███░░███ ░███
+ ░███ ░███ ░███ ░███████  ░███ ░███████  ░███ ░███
+ ░███ ░███ ░███ ░███░░░   ░███ ░███░░░   ░███ ░███
  █████░███ █████░░██████  █████░░██████  ░░████████
-░░░░░ ░░░ ░░░░░  ░░░░░░  ░░░░░  ░░░░░░    ░░░░░░░░ 
+░░░░░ ░░░ ░░░░░  ░░░░░░  ░░░░░  ░░░░░░    ░░░░░░░░
         ]],
         -- https://patorjk.com/software/taag/#p=display&f=Delta%20Corps%20Priest%201&t=meleu
         --         header = [[
@@ -44,4 +59,3 @@ return {
     },
   },
 }
-
