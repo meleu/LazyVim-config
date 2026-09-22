7165c10dc8553e3b0670e47bf607bda491d251be
meleu <8508804+meleu@users.noreply.github.com>
2026-09-22 00:24:38 -0300

    Configure treesj splitting/joining for Rego

--- lua/plugins/treesj.lua
+++ lua/plugins/treesj.lua
@@ -9,8 +9,37 @@ return {
     },
   },
   cmd = { "TSJToggle", "TSJSplit", "TSJJoin" },
-  opts = {
-    use_default_keymaps = false,
-    max_join_length = 240,
-  },
+  opts = function()
+    local lang_utils = require("treesj.langs.utils")
+    -- opa fmt style: `[1, 2]` / `{"a": 1}`, no padding inside brackets
+    local rego_list = lang_utils.set_preset_for_list({
+      join = { space_in_brackets = false },
+    })
+
+    return {
+      -----------------------------
+      -- from my original config --
+      -----------------------------
+      use_default_keymaps = false,
+      max_join_length = 240,
+      -----------------------------
+      langs = {
+        rego = {
+          array = rego_list,
+          -- the braces of a set live in `non_empty_set`, not in `set`
+          non_empty_set = rego_list,
+          object = lang_utils.set_preset_for_dict({
+            join = { space_in_brackets = false },
+          }),
+          -- call parens are siblings of `fn_args`, so treat the args as a non-bracket node
+          fn_args = lang_utils.set_preset_for_args({
+            both = { non_bracket_node = true },
+          }),
+          expr_call = {
+            target_nodes = { "fn_args" },
+          },
+        },
+      },
+    }
+  end,
 }
